5a1430e247fe57554d0fc9003dc33f2da14ab3ef
thanhleothanh <thanhng260588@gmail.com>
2022-06-29 22:40:59 +0700

    project init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename='/'>
+      <main className='h-auto min-h-screen'>
+        <Route path='/' component={WelcomeScreen} />
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomeScreen = () => {
+  return (
+    <div className='h-screen w-full bg-yellow-100'>
+      <div className='text-center'>Hello World</div>
+    </div>
+  );
+};
+
+export default WelcomeScreen;
